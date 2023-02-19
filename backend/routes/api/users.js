f026cec319696154cf9eab93a08157c76482b8f4
“Emmett <emmett.wechsler11@gmail.com>
2023-02-18 19:08:03 -0500

    user auth validations

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -7,6 +7,12 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 
+const { loginUser, restoreUser } = require('../../config/passport');
+const { isProduction } = require('../../config/keys');
+
+const validateRegisterInput = require('../../validations/register');
+const validateLoginInput = require('../../validations/login');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json({
@@ -14,66 +20,80 @@ router.get('/', function(req, res, next) {
   });
 });
 
-/* POST user log in */
-
-router.post('/login', async (req, res, next) => {
-  passport.authenticate('local', async function(err, user) {
-    if (err) return next(err);
-    if (!user) {
-      const err = new Error('Invalid credentials');
-      err.statusCode = 400;
-      err.errors = { email: 'Invalid credentials' };
-      return next(err);
-    }
-    return res.json({ user });
-  })(req, res, next);
-});
-
-/* POST user sign up */
-
-router.post('/register', async (req, res, next) => {
+// POST /api/users/register
+router.post('/register', validateRegisterInput, async (req, res, next) => {
+  // Check to make sure no one has already registered with the proposed email or
+  // username.
   const user = await User.findOne({
     $or: [{ email: req.body.email }, { username: req.body.username }]
   });
 
   if (user) {
-    const err = new Error('Validation Error');
+    // Throw a 400 error if the email address and/or username already exists
+    const err = new Error("Validation Error");
     err.statusCode = 400;
-
     const errors = {};
-
     if (user.email === req.body.email) {
-      errors.email = 'A user has already registered with this email';
+      errors.email = "A user has already registered with this email";
     }
-
     if (user.username === req.body.username) {
-      errors.username = 'A user has already registered with this username';
+      errors.username = "A user has already registered with this username";
     }
-
     err.errors = errors;
     return next(err);
   }
-});
 
-const newUser = new User({
-  username: req.body.username,
-  email: req.body.email
-});
+  // Otherwise create a new user
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email
+  });
 
-bcrypt.genSalt(10, (err, salt) => {
-  if (err) throw err;
-
-  bcrypt.hash(req.body.password, salt, async (err, hashedPassword) => {
+  bcrypt.genSalt(10, (err, salt) => {
     if (err) throw err;
+    bcrypt.hash(req.body.password, salt, async (err, hashedPassword) => {
+      if (err) throw err;
+      try {
+        newUser.hashedPassword = hashedPassword;
+        const user = await newUser.save();
+        return res.json(await loginUser(user)); // <-- THIS IS THE CHANGED LINE
+      }
+      catch (err) {
+        next(err);
+      }
+    })
+  });
+});
 
-    try {
-      newUser.hashedPassword = hashedPassword;
-      const user = await newUser.save();
-      return res.json({ user });
+// POST /api/users/login
+router.post('/login', validateLoginInput, async (req, res, next) => {
+  passport.authenticate('local', async function (err, user) {
+    if (err) return next(err);
+    if (!user) {
+      const err = new Error('Invalid credentials');
+      err.statusCode = 400;
+      err.errors = { email: "Invalid credentials" };
+      return next(err);
     }
-    catch(err) {
-      next(err);
-    }
+    return res.json(await loginUser(user)); // <-- THIS IS THE CHANGED LINE
+  })(req, res, next);
+});
+
+// routes/api/users.js
+
+router.get('/current', restoreUser, (req, res) => {
+  if (!isProduction) {
+    // In development, allow React server to gain access to the CSRF token
+    // whenever the current user information is first loaded into the
+    // React application
+    const csrfToken = req.csrfToken();
+    res.cookie("CSRF-TOKEN", csrfToken);
+  }
+  if (!req.user) return res.json(null);
+  res.json({
+    _id: req.user._id,
+    username: req.user.username,
+    email: req.user.email
   });
 });
 
